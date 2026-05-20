49e004694801339e3dcb6c1587e85d8187616336
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2026-05-20 16:54:53 +0500

    model colors

--- js/app.js
+++ js/app.js
@@ -16,4 +16,33 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // COLORS
+
+    const colorLinks = document.querySelectorAll('.color-link');
+    if(colorLinks) {
+        colorLinks.forEach(link => {
+            link.addEventListener('click', e => {
+                e.preventDefault();
+
+                if(link.classList.contains('is-active')) return;
+
+                const parent = link.closest('.model__item');
+                const modelImage = parent.querySelector('.model__item--image img');
+                const color = link.getAttribute('data-color');
+                const carImagePath = link.getAttribute('data-car-image');
+                let img = new Image();
+                img.onload = function () {
+                    // console.log(`Изображение загружено, размеры ${img.width}x${img.height}`);
+                    parent.querySelectorAll('.color-link.is-active').forEach((el) => {
+                        el.classList.remove('is-active');
+                    });
+                    parent.querySelector(`[data-color='${color}']`).classList.add('is-active');
+                    modelImage.setAttribute('src', carImagePath);
+                }
+                img.src = carImagePath;
+
+            });
+        });
+    }
+
 });
